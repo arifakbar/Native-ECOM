27c64ef7edfb9b53ab52668508f05744078fa3af
Akbar <arif.akbar@capgemini.com>
2023-03-28 18:05:04 +0530

    new SC function

--- functions/SubCategory.js
+++ functions/SubCategory.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const getAllSubCategories = async () => {
-    return await axios.get('https://cute-red-badger-tux.cyclic.app/api/sub-categories')
+    return await axios.get('https://cute-red-badger-tux.cyclic.app/api/sub-categories');
+}
+
+export const getCategoryById = async (id) => {
+    return await axios.get(`https://cute-red-badger-tux.cyclic.app/api/sub-categories/${id}`);
 }
